5772f9713ce870674cfcf498505ddfe49428c380
BWallyn <ben96.wallyn@gmail.com>
2025-08-01 08:44:55 +0200

    feat: Add nodes to create train and test datasets

--- src/impostor_hunt_in_texts/pipelines/prepare_data/pipeline.py
+++ src/impostor_hunt_in_texts/pipelines/prepare_data/pipeline.py
@@ -4,7 +4,28 @@ generated using Kedro 0.19.14
 """
 
 from kedro.pipeline import node, Pipeline, pipeline  # noqa
+from impostor_hunt_in_texts.pipelines.prepare_data.nodes import (
+    create_dataset_test,
+    create_dataset_train,
+)
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline(
+        pipe=[
+            node(
+                func=create_dataset_train,
+                inputs=["df_train", "params:path_data"],
+                outputs="dataset_train",
+                name="Create_dataset_train",
+            ),
+            node(
+                func=create_dataset_test,
+                inputs="params:path_data",
+                outputs="dataset_test",
+                name="Create_dataset_test",
+            ),
+        ],
+        inputs=["df_train"],
+        namespace="prepare_data",
+    )
