f98544e29ddd1d1d6d21624f953fce7554bcdb15
BWallyn <ben96.wallyn@gmail.com>
2025-08-08 19:10:04 +0200

    feat: Function to save Hugging Face DatasetDict

---
+++ src/impostor_hunt_in_texts/utils/utils.py
@@ -0,0 +1,22 @@
+# =================
+# ==== IMPORTS ====
+# =================
+
+from datasets import DatasetDict
+
+# ===================
+# ==== FUNCTIONS ====
+# ===================
+
+def save_hf_datasetdict(dataset_dict: DatasetDict, path_to_save: str) -> dict[str, str]:
+    """Save the Hugging Face DatasetDict and store all the infos in a dictionary.
+
+    Args:
+        dataset_dict (DatasetDict): The DatasetDict to save.
+        path_to_save (str): The path where the DatasetDict will be saved.
+
+    Returns:
+        (dict[str, str]): A dictionary containing the paths to the saved datasets in dict.
+    """
+    dataset_dict.save_to_disk(path_to_save)
+    return {key: f"{path_to_save}/{key}" for key in dataset_dict.keys()}
