23e0cf111af831b3cdde71f847b1e1ba1e45f377
BWallyn <ben96.wallyn@gmail.com>
2025-08-09 10:46:34 +0200

    chore: Update to the new kedro version

--- src/impostor_hunt_in_texts/pipelines/prepare_data/pipeline.py
+++ src/impostor_hunt_in_texts/pipelines/prepare_data/pipeline.py
@@ -1,37 +1,38 @@
 """This is a boilerplate pipeline 'prepare_data' generated using Kedro 0.19.14"""
 
-from kedro.pipeline import node, Pipeline, pipeline  # noqa
+from kedro.pipeline import Node, Pipeline
+
 from impostor_hunt_in_texts.pipelines.prepare_data.nodes import (
     create_dataset_test,
     create_dataset_train,
-    create_datasets_dict
+    create_datasets_dict,
 )
 from impostor_hunt_in_texts.utils.utils import save_hf_datasetdict
 
 
 def create_pipeline(**kwargs) -> Pipeline:
     """Create the prepare data pipeline."""
-    return pipeline(
-        pipe=[
-            node(
+    return Pipeline(
+        nodes=[
+            Node(
                 func=create_dataset_train,
                 inputs=["df_train", "params:path_data_train"],
                 outputs="dataset_train",
                 name="Create_dataset_train",
             ),
-            node(
+            Node(
                 func=create_dataset_test,
                 inputs="params:path_data_test",
                 outputs="dataset_test",
                 name="Create_dataset_test",
             ),
-            node(
+            Node(
                 func=create_datasets_dict,
                 inputs=["dataset_train", "dataset_test"],
                 outputs="dataset_dict",
                 name="Create_datasets_dict",
             ),
-            node(
+            Node(
                 func=save_hf_datasetdict,
                 inputs=["dataset_dict", "params:path_dataset_dict"],
                 outputs="dict_metadata_datasets",
