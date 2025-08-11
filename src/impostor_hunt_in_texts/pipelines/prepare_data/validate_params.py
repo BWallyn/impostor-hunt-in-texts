313795eeca205c1e2b055045bc3795ca6ee1ef5c
BWallyn <ben96.wallyn@gmail.com>
2025-08-11 12:22:41 +0200

    feat: Use the pydantic types

--- src/impostor_hunt_in_texts/pipelines/prepare_data/validate_params.py
+++ src/impostor_hunt_in_texts/pipelines/prepare_data/validate_params.py
@@ -2,7 +2,7 @@
 # ==== IMPORTS ====
 # =================
 
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, StrictStr
 
 # ===============
 # ==== CLASS ====
@@ -11,17 +11,17 @@ from pydantic import BaseModel, Field
 class ValidateParams(BaseModel):
     """Class to validate the input parameters of the prepare_data pipeline."""
 
-    path_data_train: str = Field(
+    path_data_train: StrictStr = Field(
         pattern=r"data\/[A-Za-z0-9\/\/]+",
         description="Path to the training data directory containing the text files.",
         frozen=True,
     )
-    path_data_test: str = Field(
+    path_data_test: StrictStr = Field(
         pattern=r"data\/[A-Za-z0-9\/\/]+",
         description="Path to the test data directory containing the text files.",
         frozen=True,
     )
-    path_dataset_dict: str = Field(
+    path_dataset_dict: StrictStr = Field(
         pattern=r"data\/[A-Za-z0-9\/\/]+",
         description="Path to the data directory that will contain the Huggging Face DatasetDict.",
         frozen=True,
