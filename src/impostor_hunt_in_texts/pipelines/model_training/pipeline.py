457db83c4804433aec84cc5932342412f2f6f9f5
BWallyn <ben96.wallyn@gmail.com>
2025-08-13 18:37:52 +0200

    feat: Add node to initialize model params

--- src/impostor_hunt_in_texts/pipelines/model_training/pipeline.py
+++ src/impostor_hunt_in_texts/pipelines/model_training/pipeline.py
@@ -3,8 +3,26 @@ This is a boilerplate pipeline 'model_training'
 generated using Kedro 1.0.0
 """
 
-from kedro.pipeline import Node, Pipeline  # noqa
+from kedro.pipeline import Node, Pipeline
+
+from impostor_hunt_in_texts.pipelines.model_training.nodes import (
+    initialize_model_params,
+)
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return Pipeline([])
+    """Create the model training pipeline."""
+    return Pipeline(
+        nodes=[
+            Node(
+                func=initialize_model_params,
+                inputs={
+                    "model_name": "params:model_name",
+                    "params": "params:model_params",
+                },
+                outputs="model_params",
+                name="Initialize_model_parameters",
+            )
+        ],
+        namespace="model_training",
+    )
