a5a5aa533e7aea7797a66529ffd8a6bf860b7ae0
BWallyn <ben96.wallyn@gmail.com>
2025-08-19 15:43:58 +0200

    feat: Function to make predictions

--- src/impostor_hunt_in_texts/pipelines/model_prediction/nodes.py
+++ src/impostor_hunt_in_texts/pipelines/model_prediction/nodes.py
@@ -2,3 +2,26 @@
 This is a boilerplate pipeline 'model_prediction'
 generated using Kedro 1.0.0
 """
+# =================
+# ==== IMPORTS ====
+# =================
+
+import pandas as pd
+from sklearn.pipeline import Pipeline
+
+# ===================
+# ==== FUNCTIONS ====
+# ===================
+
+def make_prediction(model: Pipeline, df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Make predictions using the trained model.
+
+    Args:
+        model (Pipeline): The trained model pipeline.
+        df (pd.DataFrame): DataFrame containing the features for prediction.
+
+    Returns:
+        (np.array): Array of the predicted labels.
+    """
+    return model.predict(df)
