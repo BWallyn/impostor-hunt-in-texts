273983edf653f5e0569deeac003c8a8ee839fef2
BWallyn <ben96.wallyn@gmail.com>
2025-08-24 16:07:46 +0200

    feat: Function to log model pipeline in a scikit-learn pipeline

--- src/impostor_hunt_in_texts/utils/utils_model.py
+++ src/impostor_hunt_in_texts/utils/utils_model.py
@@ -20,3 +20,15 @@ def load_model(model_id: str) -> Pipeline:
         (Pipeline): The loaded model pipeline.
     """
     return mlflow.pyfunc.load_model(f"models:/{model_id}")
+
+
+def load_model_sklearn(model_id: str) -> Pipeline:
+    """Load the trained scikit-learn model from MLflow.
+
+    Args:
+        model_id (str): The ID of the model in MLflow.
+
+    Returns:
+        (Pipeline): The loaded scikit-learn model pipeline.
+    """
+    return mlflow.sklearn.load_model(f"models:/{model_id}")
