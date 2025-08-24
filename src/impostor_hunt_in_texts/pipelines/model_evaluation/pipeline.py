f39008017efcd00118535ae567767f4f22c04670
BWallyn <ben96.wallyn@gmail.com>
2025-08-24 16:09:07 +0200

    feat: Log model and compute feature importance

--- src/impostor_hunt_in_texts/pipelines/model_evaluation/pipeline.py
+++ src/impostor_hunt_in_texts/pipelines/model_evaluation/pipeline.py
@@ -3,8 +3,44 @@ This is a boilerplate pipeline 'model_evaluation'
 generated using Kedro 1.0.0
 """
 
-from kedro.pipeline import Node, Pipeline  # noqa
+from kedro.pipeline import Node, Pipeline
+
+from impostor_hunt_in_texts.pipelines.model_evaluation.nodes import (
+    get_feature_importance,
+    log_tables_to_mlflow,
+)
+from impostor_hunt_in_texts.utils.utils_model import load_model_sklearn
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return Pipeline([])
+    """Create the model evaluation pipeline."""
+    return Pipeline(
+        nodes=[
+            Node(
+                func=load_model_sklearn,
+                inputs="model_id",
+                outputs="model",
+                name="Load_trained_model",
+            ),
+            Node(
+                func=get_feature_importance,
+                inputs={
+                    "model": "model",
+                    "df": "df_test_features",
+                },
+                outputs="dict_tables_to_log",
+                name="Get_feature_importance",
+            ),
+            Node(
+                func=log_tables_to_mlflow,
+                inputs={
+                    "dict_tables": "dict_tables_to_log",
+                    "run_id": "run_id",
+                },
+                outputs=None,
+                name="Log_tables_to_MLflow",
+            )
+        ],
+        namespace="model_evaluation",
+        inputs=["model_id", "df_test_features", "run_id"],
+    )
